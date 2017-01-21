e8c6fff9ba8dbc47c248233ba727c25d51f62591
JamesG <j.gronowski@gmail.com>
2017-01-21 16:24:32 +1100

    input manager changes, player head prefabs and script to move them

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+
+    public int joystick;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localEulerAngles = new Vector3(-Input.GetAxis("L_YAxis_" + joystick) * 30,0, -Input.GetAxis("L_XAxis_" + joystick) * 30);
+
+    }
+}
