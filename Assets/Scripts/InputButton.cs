a0171a111e1aabaca66ee57055429266aeba8821
James <jsmchugh@gmail.com>
2017-01-22 09:53:17 +1100

    Beginnings of new input system.

---
+++ Assets/Scripts/InputButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Button", menuName = "Hair/Input Button")]
+public class InputButton : ScriptableObject
+{
+	public string buttonName;
+
+	public List<KeyCode> keyCodes;
+}
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+	InputButton[] inputButtons;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Object[] buttonObjects = Resources.LoadAll ("", typeof(InputButton));
+		Debug.Log ("Found buttons: " + buttonObjects.Length);
+		inputButtons = new InputButton[buttonObjects.Length];
+		for(int i = 0; i < buttonObjects.Length; i++)
+		{
+			inputButtons [i] = (InputButton) buttonObjects [i];
+			Debug.Log ("Input Button: " + inputButtons[i].buttonName);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		foreach (InputButton inputButton in inputButtons)
+		{
+			for (int i = 0; i < 2; i++)
+			{
+				if (Input.GetButtonDown (inputButton.buttonName + (i + 1)))
+				{
+					Debug.Log ("P" + (i + 1) + " pressed " + inputButton.buttonName + (i + 1));
+				}
+			}
+		}
+	}
+}
