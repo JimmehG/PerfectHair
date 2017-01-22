b7292781ba13f0ca93f94c1c9ab0c57f08f311bb
James <jsmchugh@gmail.com>
2017-01-22 14:30:33 +1100

    Heart Manager and sprites.

---
+++ Assets/Scripts/Hearts.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hearts : MonoBehaviour
+{
+    [SerializeField]
+    int maxHearts;
+
+    int currentHearts;
+
+    [SerializeField]
+    Image heartPrefab;
+
+    [SerializeField]
+    Sprite fullSprite;
+
+    Image[] hearts;
+
+    void Start()
+    {
+        currentHearts = 0;
+        hearts = new Image[maxHearts];
+        for (int i = 0; i < maxHearts; i++)
+        {
+            Image newHeart = Object.Instantiate<Image>(heartPrefab, this.transform);
+            newHeart.gameObject.name = "heart" + i;
+
+            Vector3 position = new Vector3(0, 0, 0);
+            position.x = 30 * i;
+            Debug.Log(i + ": " + position.x);
+            newHeart.transform.localPosition = position;
+            hearts[i] = newHeart;
+        }
+    }
+
+    public void AddHeart()
+    {
+        if (currentHearts < maxHearts)
+            hearts[currentHearts++].sprite = fullSprite;
+    }
+}
