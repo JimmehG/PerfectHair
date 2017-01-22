bb89b8110a3f93077b30dbfc228e5e4b737ca2b3
jamesg2ndcomp <asdgjk@gaskdf.com>
2017-01-22 14:40:58 +1100

    :)

---
+++ Assets/Scripts/Flicker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flicker : MonoBehaviour
+{
+    Text text;
+
+    public bool flickering = false;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        StartCoroutine(FlickerText());
+    }
+
+    private IEnumerator FlickerText()
+    {
+        flickering = true;
+        while (flickering)
+        {
+            if (text.enabled)
+            {
+                text.enabled = false;
+            }
+            else
+            {
+                text.enabled = true;
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
