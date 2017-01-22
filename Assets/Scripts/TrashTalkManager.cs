d705b3ac5d96d080d49b9e03d611bd998ffbaa24
James <jsmchugh@gmail.com>
2017-01-22 14:52:08 +1100

    TrashtalkManager

---
+++ Assets/Scripts/TrashTalkManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashTalkManager : MonoBehaviour
+{
+    Trashtalk[] trashTalk;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Object[] trashObject = Resources.LoadAll("", typeof(Trashtalk));
+        Debug.Log("Found trashtalk: " + trashObject.Length);
+        trashTalk = new Trashtalk[trashObject.Length];
+        for (int i = 0; i < trashObject.Length; i++)
+        {
+            trashTalk[i] = (Trashtalk)trashObject[i];
+            Debug.Log("Trashtalk: " + trashTalk[i].talk);
+        }
+    }
+
+    public string getRandomTrashTalk()
+    {
+        return trashTalk[Random.Range(0, trashTalk.Length)].talk;
+    }
+}
